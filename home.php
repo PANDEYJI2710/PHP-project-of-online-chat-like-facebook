9dfc66005ad88452a79ff1393c7ddd547d203a6c
Bunty Pandey <pandeysatyam133@gmail.com>
2020-03-26 12:50:39 +0530

    Login Page Suucessfully Connected To Database

---
+++ home.php
@@ -0,0 +1,21 @@
+<htmL>
+<head>
+    <title>MY CHAT - HOME</title>
+    <link rel= "stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    </head>
+<body>
+    <div class= "container main-section">
+        <div class= "row">
+            <div class= "col-md-3 col-sm-3 col-xs-12 left sidebar">
+                <div class="input-group-btn">
+                    <div class="input-gorup-btn">
+                        <center><a href="include/find_friends.php"><button class="btn btn-default
+                        search-icon" name="search_user" type="submit">Add new user</button></a></center>
+                </div>
+            </div>
+        </div>
+    </div>
+</html>
