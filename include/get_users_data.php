ab5b73663343ef343ff9c7690fe9c6ff2b1fc19a
Bunty Pandey <pandeysatyam133@gmail.com>
2020-03-26 16:34:14 +0530

    Home Page Created

---
+++ include/get_users_data.php
@@ -0,0 +1,27 @@
+<?php 
+
+
+$con = mysqli_connect("localhost", "root", "", "mychat");
+
+    $user= "select * from users";
+
+    $run_user= mysqli_query($con, $user);
+
+    while ($row_user=mysqli_fetch_array($run_user)){
+        $user_id = $row_user['user_id'];
+        $user_name = $row_user['user_name'];
+        $user_profile = $row_user['user_profile'];
+        $login = $row_user['log_in'];
+
+        echo"
+        <li>
+        <div>
+        </div>
+        </li>
+
+
+        ";
+    }
+
+
+?>
